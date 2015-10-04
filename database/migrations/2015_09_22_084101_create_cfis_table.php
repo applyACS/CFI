3d997026333763121aca06c1c30b93c9bfe25a14
Alexandru Cristian <alex.cristi19@gmail.com>
2015-10-05 00:24:55 +0300

    migration class changed

--- database/migrations/2015_09_22_084101_create_cfis_table.php
+++ database/migrations/2015_09_22_084101_create_cfis_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CfiInscris extends Migration
+class CreateCfisTable extends Migration
 {
     /**
      * Run the migrations.
